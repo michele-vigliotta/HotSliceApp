184fd7700423ad3ba36b343072a1ab1974ad2daf
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-05-16 18:13:35 +0200

    check del ruolo lesgo

--- app/src/main/java/com/example/hotsliceapp/MainActivity.kt
+++ app/src/main/java/com/example/hotsliceapp/MainActivity.kt
@@ -1,30 +1,36 @@
 package com.example.hotsliceapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
 
-    val firestore = Firebase.firestore
+    val db = Firebase.firestore
 
+    private lateinit var auth: FirebaseAuth
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         setContentView(R.layout.activity_main)
 
-
+        auth = Firebase.auth
 
         val intent = Intent(this, Login::class.java)
 
@@ -32,16 +38,29 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val city = hashMapOf(
-            "name" to "Los Angeles",
-            "state" to "CA",
-            "country" to "US",
-        )
+        (findViewById<Button>(R.id.button2)).setOnClickListener {
 
-        firestore.collection("cities").document("LA")
-            .set(city)
-            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+            val authid = (auth.currentUser?.uid).toString()
+            val documentSnapshot = db.collection("users").document(authid)
+            documentSnapshot.get().addOnSuccessListener {
+                document ->
+                val role = document.getString("role")
+                Toast.makeText(baseContext, "Role: $role", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+        (findViewById<Button>(R.id.button3)).setOnClickListener {
+
+            auth.signOut()
+            Toast.makeText(baseContext, "Utente Disconnesso", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+
+
 
 
 
