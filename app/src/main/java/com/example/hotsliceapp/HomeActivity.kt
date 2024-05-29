006278de3eee40e2d4b671e346a6ac0077a7687f
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-05-29 18:19:01 +0200

    Aggiunta controllo ruolo utente per la vista HomeActivity

--- app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
+++ app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
@@ -1,16 +1,39 @@
 package com.example.hotsliceapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    val db = Firebase.firestore
+    lateinit var role: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        auth = Firebase.auth
+
+        val authid = (auth.currentUser?.uid).toString()
+        val documentSnapshot = db.collection("users").document(authid)
+        documentSnapshot.get().addOnSuccessListener {
+                document ->
+             role = document.getString("role").toString()
+            if (role == "staff") {
+                var btnmenu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+                btnmenu.menu.clear()
+                btnmenu.inflateMenu(R.menu.bottom_menu_staff)
+
+            }
+            else if (role == "admin"){
+                var btnmenu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+                btnmenu.menu.clear()
+                btnmenu.inflateMenu(R.menu.bottom_menu_admin)
+            }
+
+        }
     }
 }
-//123
