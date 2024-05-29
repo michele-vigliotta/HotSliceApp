05cfc19f8ba62b022c353b0268818cdd99fc8656
Giovanni <giovanniprati18@gmail.com>
2024-05-29 17:01:50 +0200

    aggiunta nav bar e activity home pt2

---
+++ app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.hotsliceapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+    }
+}
+//123
