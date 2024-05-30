0c15078f4153d5da9e6281d2d9bd6d2ad603e1a2
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-05-30 21:56:51 +0200

    Small changes and comments

--- app/src/main/java/com/example/hotsliceapp/Login.kt
+++ app/src/main/java/com/example/hotsliceapp/Login.kt
@@ -1,14 +1,10 @@
 package com.example.hotsliceapp
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
