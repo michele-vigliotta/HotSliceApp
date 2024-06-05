80ba836b18d2cfde6ec038ad781ecad565ecaa71
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-06-05 15:39:25 +0200

    Tolto setconteview ricorsivo

--- app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
+++ app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
@@ -20,7 +20,6 @@ class HomeActivity : AppCompatActivity() {
     lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
         binding=ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
