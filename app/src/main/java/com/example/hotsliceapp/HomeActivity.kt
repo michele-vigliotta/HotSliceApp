7be7a617e1f98fc77ff718fd73a7bf5d7ae11757
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-06-05 21:45:09 +0200

    Qualche modifica estetica

--- app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
+++ app/src/main/java/com/example/hotsliceapp/HomeActivity.kt
@@ -2,10 +2,9 @@ package com.example.hotsliceapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.hotsliceapp.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
@@ -45,20 +44,20 @@ class HomeActivity : AppCompatActivity() {
                 document ->
              role = document.getString("role").toString()
             if (role == "staff") {
-                var bottom_menu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+                val bottom_menu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
                 bottom_menu.menu.clear()
                 bottom_menu.inflateMenu(R.menu.bottom_menu_staff)
 
             }
             else if (role == "admin"){
-                var btnmenu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+                val btnmenu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
                 btnmenu.menu.clear()
                 btnmenu.inflateMenu(R.menu.bottom_menu_admin)
             }
         }
 
         //codice per il logout
-        var logout_btn= findViewById<Button>(R.id.logout_button)
+        val logout_btn= findViewById<ImageButton>(R.id.logout_button)
         logout_btn.setOnClickListener {
             auth.signOut()
             Toast.makeText(baseContext, "Utente Disconnesso", Toast.LENGTH_SHORT)
@@ -67,13 +66,5 @@ class HomeActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
-    }
-
-    private fun replaceFragment(fragment: Fragment) {//funzione per mettere un fragment nel fragment container
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainerView,fragment)
-        fragmentTransaction.commit()
     }
 }
