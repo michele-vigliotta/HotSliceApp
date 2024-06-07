86453cf2dc291d1fcd1666fa4e8e93b53d05ebf7
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-06-07 13:12:08 +0200

    Aggiunta logica per carrello da vista dettagli

--- app/src/main/java/com/example/hotsliceapp/activities/DettagliProdottoActivity.kt
+++ app/src/main/java/com/example/hotsliceapp/activities/DettagliProdottoActivity.kt
@@ -1,8 +1,11 @@
 package com.example.hotsliceapp.activities
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -35,5 +38,39 @@ class DettagliProdottoActivity : AppCompatActivity() {
         } else {
                 imageView.setImageResource(R.drawable.pizza_foto)
                 }
+        var editTextQuantity : EditText = findViewById(R.id.editText_quantity)
+        var buttonMinus : Button = findViewById(R.id.button_minus)
+        var buttonAdd : Button = findViewById(R.id.button_plus)
+        var buttonAddToCart : Button = findViewById(R.id.button_add_to_cart)
+
+        editTextQuantity.setText("0")
+
+            buttonMinus.setOnClickListener {
+                var currentQuantity = editTextQuantity.text.toString().toInt()
+                if (currentQuantity > 0) {
+                currentQuantity--
+                editTextQuantity.setText(currentQuantity.toString())
+                }
+
+            buttonAdd.setOnClickListener {
+                var currentQuantity = editTextQuantity.text.toString().toInt()
+                currentQuantity++
+                editTextQuantity.setText(currentQuantity.toString())
+                }
+
+            buttonAddToCart.setOnClickListener {
+                var currentQuantity = editTextQuantity.text.toString().toInt()
+                if (currentQuantity > 0) {
+                    if (item != null) {
+                        addToCart(item.nome, currentQuantity)
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun addToCart(itemName: String, quantity: Int) {
+        Toast.makeText(this, "Item added to cart: $itemName", Toast.LENGTH_SHORT).show()
     }
 }
