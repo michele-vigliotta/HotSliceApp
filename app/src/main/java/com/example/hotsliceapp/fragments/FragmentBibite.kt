58aceda45f6efae15709935cd9ccf73f1207a389
Giovanni Prati <94047126+jjohnnyUtah@users.noreply.github.com>
2024-06-09 23:23:54 +0200

    Update FragmentBibite.kt

--- app/src/main/java/com/example/hotsliceapp/fragments/FragmentBibite.kt
+++ app/src/main/java/com/example/hotsliceapp/fragments/FragmentBibite.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,7 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class FragmentBibite:Fragment() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var AdapterBibite: AdapterListeHome
+    private lateinit var bibiteAdapter: AdapterListeHome
     private val bibiteList = mutableListOf<Item>()
     private val REQUEST_CODE_DETTAGLI = 100
 
@@ -27,16 +28,16 @@ class FragmentBibite:Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.bibite_fragment,container,false)
+        val view = inflater.inflate(R.layout.bibite_fragment, container, false)
 
         recyclerView = view.findViewById(R.id.recyclerBibite)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        AdapterBibite = AdapterListeHome(bibiteList)
-        recyclerView.adapter = AdapterBibite
+        bibiteAdapter = AdapterListeHome(bibiteList)
+        recyclerView.adapter = bibiteAdapter
         fetchDataFromFirebase()
 
-        AdapterBibite.onItemClick = {
+        bibiteAdapter.onItemClick = {
             val intent = Intent(activity, DettagliProdottoActivity::class.java)
             intent.putExtra("item", it)
             startActivityForResult(intent, REQUEST_CODE_DETTAGLI)
@@ -63,10 +64,19 @@ class FragmentBibite:Fragment() {
                     val bibita = document.toObject(Item::class.java)
                     bibiteList.add(bibita)
                 }
-                AdapterBibite.notifyDataSetChanged()
+                bibiteAdapter.notifyDataSetChanged()
             }
             .addOnFailureListener { exception ->
                 Log.w("BibiteFragment", "Error getting documents.", exception)
             }
+    }
+
+    fun filterList(query: String) {//metodo che filtra la lista quando si utilizza la searchview
+        val filteredList = bibiteList.filter { it.nome.contains(query, ignoreCase = true) }
+        bibiteAdapter.setFilteredList(filteredList)
+        if (filteredList.isEmpty()) {
+            // Se l'elenco filtrato è vuoto, mostra il messaggio
+            Toast.makeText(context, "Bibita non presente nel menù", Toast.LENGTH_SHORT).show()
+        }
     }
 }
