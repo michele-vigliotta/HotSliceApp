2f2d47eafa5988d8a4c155607158178777925aff
Giovanni Prati <94047126+jjohnnyUtah@users.noreply.github.com>
2024-06-09 23:22:51 +0200

    Update FragmentPizza.kt

--- app/src/main/java/com/example/hotsliceapp/fragments/FragmentPizza.kt
+++ app/src/main/java/com/example/hotsliceapp/fragments/FragmentPizza.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,8 +20,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class FragmentPizza:Fragment() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var pizzaAdapter: AdapterListeHome
-    private val pizzaList = mutableListOf<Item>()
+    lateinit var pizzaAdapter: AdapterListeHome
+    var pizzaList = mutableListOf<Item>()
     private val REQUEST_CODE_DETTAGLI = 100
 
 
@@ -28,7 +29,7 @@ class FragmentPizza:Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.pizza_fragment,container,false)
+        val view = inflater.inflate(R.layout.pizza_fragment, container, false)
 
         recyclerView = view.findViewById(R.id.recyclerPizze)
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -55,25 +56,31 @@ class FragmentPizza:Fragment() {
         }
     }
 
-
     private fun fetchDataFromFirebase() {
 
-            val db = FirebaseFirestore.getInstance()
-            db.collection("pizze")
-                .get()
-                .addOnSuccessListener { result ->
-                    for (document in result) {
-                        //converte ogni elemento in un oggetto e lo aggiunge alla lista
-                        val pizza = document.toObject(Item::class.java)
-                        pizzaList.add(pizza)
-                    }
-                    //aggiorna l'adapter con la nuova lista
-                    pizzaAdapter.notifyDataSetChanged()
+        val db = FirebaseFirestore.getInstance()
+        db.collection("pizze")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    //converte ogni elemento in un oggetto e lo aggiunge alla lista
+                    val pizza = document.toObject(Item::class.java)
+                    pizzaList.add(pizza)
                 }
-                .addOnFailureListener { exception ->
-                    Log.w("PizzaFragment", "Error getting documents.", exception)
-                }
-        }
-
-
+                //aggiorna l'adapter con la nuova lista
+                pizzaAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.w("PizzaFragment", "Error getting documents.", exception)
+            }
     }
+
+    fun filterList(query: String) {//metodo che filtra la lista quando si utilizza la searchview
+        val filteredList = pizzaList.filter { it.nome.contains(query, ignoreCase = true) }
+        pizzaAdapter.setFilteredList(filteredList)
+        if (filteredList.isEmpty()) {
+            // Se l'elenco filtrato è vuoto, mostra il messaggio
+            Toast.makeText(context, "Pizza non presente nel menù", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
