30c1dda59f410025495ae1b967289d79f50bca86
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-07-12 21:53:54 +0200

    Migliorie register e aggiunta conferma password

--- app/src/main/java/com/example/hotsliceapp/activities/Register.kt
+++ app/src/main/java/com/example/hotsliceapp/activities/Register.kt
@@ -5,12 +5,18 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.hotsliceapp.R
 import com.example.hotsliceapp.databinding.ActivityRegisterBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class Register: AppCompatActivity() {
@@ -23,6 +29,8 @@ class Register: AppCompatActivity() {
 
     private lateinit var layoutRegister: View
     private lateinit var layoutNoInternet: View
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +45,14 @@ class Register: AppCompatActivity() {
         connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         registerNetworkCallback() // Registra il NetworkCallback all'avvio
         checkInternetConnection() // Verifica la connessione iniziale
+
+        val textView = findViewById<TextView>(R.id.AlreadyRegisteredText)
+        val spannableString = SpannableString(textView.text)
+        val color1 = ContextCompat.getColor(this, R.color.black)
+        val color2 = ContextCompat.getColor(this, R.color.red)
+        spannableString.setSpan(ForegroundColorSpan(color1), 0, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(color2), 20, spannableString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = spannableString
     }
 
     private fun checkInternetConnection(){
@@ -49,32 +65,35 @@ class Register: AppCompatActivity() {
             binding.btnRegister.setOnClickListener {
                 val email = binding.etEmail.text.toString()
                 val passw = binding.etPassword.text.toString()
+                val confirmPassw = binding.etConfermaPassword.text.toString()
 
-                if (email.isNotEmpty() && passw.isNotEmpty()) {
-                    firebaseAuth.createUserWithEmailAndPassword(email, passw)
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                Toast.makeText(
-                                    this,
-                                    "Registrazione avvenuta con successo",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                val intent = Intent(this, Login::class.java)
-                                startActivity(intent)
-                                finish() //chiude la vecchia activity appena siamo nella nuova
+                if (email.isNotEmpty() && passw.isNotEmpty() && confirmPassw.isNotEmpty()) {
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                        Snackbar.make(binding.root, "Email non valida", Snackbar.LENGTH_SHORT).show()
+                    } else if (passw.length < 6) {
+                        Snackbar.make(binding.root, "La password deve contenere almeno 6 caratteri", Snackbar.LENGTH_SHORT).show()
+                    } else if (passw != confirmPassw) {
+                        Snackbar.make(binding.root, "Le password inserite non coincidono", Snackbar.LENGTH_SHORT).show()
+                    } else {
+                        // Tutte le condizioni sono soddisfatte, procedi con la registrazione
+                        firebaseAuth.createUserWithEmailAndPassword(email, passw)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    Toast.makeText(
+                                        this,
+                                        "Registrazione avvenuta con successo",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    val intent = Intent(this, Login::class.java)
+                                    startActivity(intent)
+                                    finish() //chiude la vecchia activity appena siamo nella nuova
+                                } else {
+                                    Snackbar.make(binding.root, "Email già in uso", Snackbar.LENGTH_SHORT).show()
+                                }
                             }
-                            else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                                Toast.makeText(this, "Formato Email non valido", Toast.LENGTH_SHORT).show()
-                            }
-                            else if(passw.length < 6){
-                                Toast.makeText(this, "La password deve contenere almeno 6 caratteri", Toast.LENGTH_SHORT).show()
-                            }
-                            else {
-                                Toast.makeText(this, "Email già in uso", Toast.LENGTH_SHORT).show()
-                            }
-                        }
+                    }
                 } else {
-                    Toast.makeText(this, "Compilare tutti i campi", Toast.LENGTH_SHORT).show()
+                    Snackbar.make(binding.root, "Compilare tutti i campi", Snackbar.LENGTH_SHORT).show()
                 }
             }
             binding.AlreadyRegisteredText.setOnClickListener{
