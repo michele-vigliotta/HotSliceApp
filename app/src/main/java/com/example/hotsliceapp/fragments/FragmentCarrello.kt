a24d8422d9eed0f196db0573a513d7de12af1ea1
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-07-13 19:35:27 +0200

    Fix descrizione ordini

--- app/src/main/java/com/example/hotsliceapp/fragments/FragmentCarrello.kt
+++ app/src/main/java/com/example/hotsliceapp/fragments/FragmentCarrello.kt
@@ -119,7 +119,7 @@ class FragmentCarrello : Fragment(), FragmentRitiroDialog.RitiroDialogListener {
             var totale: Double = 0.0
             for (item in newList) {
                 totale += (item.quantita * item.prezzo)
-                string += "${item.quantita} ${item.nome},\n"
+                string += "${item.quantita} ${item.nome};"
             }
             val nuovoOrdine = hashMapOf(
                 "id" to "${authid}_${System.currentTimeMillis()}",
